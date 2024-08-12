239c19975f38e4b1872886ab22ef6e7b75af4be0
Vagner-Alves <vagneralves997@gmail.com>
2024-08-12 08:18:44 -0300

    excelente segunda feira

---
+++ classroom/App_copy.vue
@@ -0,0 +1,47 @@
+<template>
+    <h1>{{name}}</h1>
+    <p v-if="status ==='active'">user active</p>
+    <p v-else-if="status ==='pending'"> user pending</p>
+    <p v-else>user inactive</p>
+  
+    <h2>Tasks to be done</h2>
+    <ul>
+      <li v-for="tasks in task" :key="task">{{ tasks }}</li>
+    </ul>
+    <!-- <a v-bind:href="link">Google search</a> -->
+     <a :href="link">Google Search</a>
+     <!-- <button v-on:click="toogleStatus">change Status</button> -->
+      <br>
+     <button @click="toogleStatus">change Status</button>
+  </template>
+  
+  <script>
+    export default{
+      data (){
+        return{
+          name: "Vagner Alves",
+          status: 'pending',
+          task: ['task one', 'task two', 'task three'],
+          link: 'https://www.google.com'
+  
+        };
+      },
+  
+      methods: {
+        toogleStatus(){
+          if (this.status === 'active'){
+            this.status = 'pending'
+          }
+  
+          else if (this.status === 'pending'){
+            this.status = 'inactive'
+          }
+  
+          else {
+            this.status = 'active'
+          }
+        }
+      }
+    }  
+  </script>
+  
