43272947055e364aedf5cc190384a1b2bcaf2d65
Vagner-Alves <vagneralves997@gmail.com>
2024-08-12 09:18:25 -0300

    deleting tasks

--- classroom/src/App.vue
+++ classroom/src/App.vue
@@ -3,10 +3,19 @@
   <p v-if="status ==='active'">user active</p>
   <p v-else-if="status ==='pending'"> user pending</p>
   <p v-else>user inactive</p>
-
+  
+  <form @submit.prevent="addTask"> 
+    <label for="newTask"> add Task</label>
+    <input type="text" id = "newTask" name="newTask" v-model="newTask">
+    <button type="submmit">submmit</button>
+  </form>
+  
   <h2>Tasks to be done</h2>
   <ul>
-    <li v-for="tasks in task" :key="task">{{ tasks }}</li>
+    <li v-for="(task, index) in tasks" :key="task">
+      <span>{{ task }}</span>
+      <button @click="deleteTask(index)">x</button>
+    </li>
   </ul>
   <!-- <a v-bind:href="link">Google search</a> -->
    <a :href="link">Google Search</a>
@@ -15,12 +24,13 @@
    <button @click="toogleStatus">change Status</button>
 </template>
 
-<script>
+<script setup>
 import {ref} from 'vue';
 
 const name = ref("Vagner Alves");
 const status = ref ('pending');
-const task =  ref(['task one', 'task two', 'task three']);
+const tasks =  ref(['task one', 'task two', 'task three']);
+const newTask =  ref('')
 
 const toogleStatus = () => {
     if ( status.value === 'active'){
@@ -34,7 +44,12 @@ const toogleStatus = () => {
     else {
         status.value = 'active'
       }
-      };
-      
-    
+    };
+
+const addTask = () => {
+  if (newTask.value.trim() !== ''){
+    tasks.value.push(newTask.value);
+    newTask.value = '';
+  }
+}
 </script>
