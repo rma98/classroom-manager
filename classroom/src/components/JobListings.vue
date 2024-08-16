2933c77cb563136ad6d3a07eeee35601457a50d9
Vagner-Alves <vagneralves997@gmail.com>
2024-08-16 20:36:19 -0300

    excelente sexta feira

--- classroom/src/components/JobListings.vue
+++ classroom/src/components/JobListings.vue
@@ -1,4 +1,5 @@
 <script setup>
+    import JobListing from './JobListing.vue';
     import JobData from '@/jobs.json';
     import {ref} from 'vue';
 
@@ -10,11 +11,14 @@
 <template>
     <section class="bg-blue-50 px-4 py-10">
         <div class="container-xl lg:container m-auto">
-            <h2 class="text-3xl font-bold text-green-500 mb-6 text-center"></h2>
+            <h2 class="text-3xl font-bold text-green-500 mb-6 text-center"> browser jobs</h2>
         </div>
-    </section>
-    
-      <div v-for="job in jobs" :key="job.id">{{ job.title }}</div>
-    
+
+        <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+            <JobListing v-for="job in jobs" :key="job.id":job="job"/>
+                {{  }}
+            <!-- <div v-for="job in jobs" :key="job.id">{{ job.title }}</div> -->    
+        </div>
+    </section>   
 
 </template>
