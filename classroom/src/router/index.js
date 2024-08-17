529dfbd786448acb894f3c929cef80dc54b267ec
Vagner-Alves <vagneralves997@gmail.com>
2024-08-16 22:41:38 -0300

    VUE ROUTE

--- classroom/src/router/index.js
+++ classroom/src/router/index.js
@@ -3,6 +3,15 @@ import {createRouter, createWebHistory} from 'vue-router';
 import HomeView from '@/views/HomeView.vue';
 
 createRouter = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL)
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: HomeView
+        },
+    ],
 
-})
+});
+
+export default router;
