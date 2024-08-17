f87ed91ed4dea7a961041e942c766cf00ef83ee7
Vagner-Alves <vagneralves997@gmail.com>
2024-08-17 20:23:20 -0300

    vue route

--- classroom/src/views/NotFoundView.vue
+++ classroom/src/views/NotFoundView.vue
@@ -1,17 +1,17 @@
 <script setup>
     import { RouterLink } from 'vue-router';
-
+    
 </script>
 
 <template>
+     
      <section class="text-center flex flex-col justify-center items-center h-96">
-      <i class="pi pi-exclamation-triangle"></i>
+      <i class="pi pi-exclamation-triangle text-yellow-400 text-7xl mb-5"></i>
       <h1 class="text-6xl font-bold mb-4">404 Not Found</h1>
-      <p class="text-xl mb-5">Essa Pagina não existe</p>
+      <p class="text-xl mb-5">This page does not exist</p>
       <RouterLink
         to="/"
         class="text-white bg-green-700 hover:bg-green-900 rounded-md px-3 py-2 mt-4"
-        >Voltar</RouterLink
-      >
+        >Go Back</RouterLink>
     </section>
 </template>
