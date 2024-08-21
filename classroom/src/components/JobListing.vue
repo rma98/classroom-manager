7c008f2ed998718ee28ed6186121df2d8b486b4e
Vagner-Alves <vagneralves997@gmail.com>
2024-08-21 00:21:45 -0300

    pt-br

--- classroom/src/components/JobListing.vue
+++ classroom/src/components/JobListing.vue
@@ -52,7 +52,7 @@
                   :to="'/jobs/' + job.id"
                   class="h-[36px] bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg text-center text-sm"
                 >
-                  ler mais
+                  conferir
                 </RouterLink>
               </div>
             </div>
